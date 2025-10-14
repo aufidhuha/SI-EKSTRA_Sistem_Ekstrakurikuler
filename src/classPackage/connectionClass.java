1ced05ba5eb3eb275a0c65f1703383541a24651c
Hanismatul <isma86756>
2025-10-14 09:58:59 +0700

    siswaclass

--- src/classPackage/connectionClass.java
+++ src/classPackage/connectionClass.java
@@ -11,9 +11,4 @@ import java.sql.Connection;
  * @author ASUS
  */
 public class connectionClass {
-
-    Connection getConnection() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-    
 }
